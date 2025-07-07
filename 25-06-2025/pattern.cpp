5b474b3a37e95949180cc489c731545974bf33dd
sundaram-vishwakarma <sundaram.sharma@geeksforgeeks.org>
2025-07-07 19:17:36 +0530

    Create pattern.cpp

---
+++ 25-06-2025/pattern.cpp
@@ -0,0 +1,76 @@
+1.	Print the below format
+1
+2   2
+3   3   3
+4   4   4    4
+5   5    5    5    5
+
+
+2.	Print the below format
+1
+1	2
+1    2    3
+1    2    3     4
+1    2    3     4      5
+
+  
+3.	Print the below format
+1
+2    2     2
+3    3     3     3    3
+4    4     4     4    4    4     4
+
+
+4.	Print the below format
+1    2     3    4     5     6      7
+1    2     3    4     5 
+1    2     3
+1
+
+
+5.	
+a
+a      b  
+a      b       c
+a      b       c        d
+a      b       c        d        e
+
+
+
+6.	
+*      *       *        *
+*      *       *        *
+*      *       *        *
+
+  
+7.	Inverted Pyramid
+*      *      *      *
+*      *      *
+*      * 
+*       
+
+  
+8. 
+5
+4       5
+3       4      5
+2       3      4       5
+1       2      3       4        5
+
+  
+9.  
+5      4        3        2       1
+4      3        2        1
+3	     2        1
+2      1
+1
+
+  
+10. 
+*
+*        *
+*                  *
+*                           *
+*         *        *        *        *
+           
+
