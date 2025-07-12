1125773ad4949750da8c6e4a39f44e8b309a5a9c
sundaram-vishwakarma <sundaram.sharma@geeksforgeeks.org>
2025-07-12 20:52:33 +0530

    Create isPalindrome.cpp

---
+++ 27-06-2025/isPalindrome.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, reverse = 0, original, remainder;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    original = n;
+    while (n != 0) {
+        remainder = n % 10;
+        reverse = reverse * 10 + remainder;
+        n /= 10;
+    }
+
+    if (original == reverse)
+        cout << original << " is a palindrome number." << endl;
+    else
+        cout << original << " is not a palindrome number." << endl;
+
+    return 0;
+}
+
