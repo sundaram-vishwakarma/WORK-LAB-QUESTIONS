e2829de5d060a5258b619f6642a0891d68d5b1cc
sundaram-vishwakarma <sundaram.sharma@geeksforgeeks.org>
2025-07-13 13:26:13 +0530

    Update LC74_searchA2dMatrix.cpp

--- 02-07-2025/LC74_searchA2dMatrix.cpp
+++ 02-07-2025/LC74_searchA2dMatrix.cpp
@@ -1,1 +1,31 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = matrix[0].size();
 
+        if (m == 0 || n == 0) {
+            return false;
+        }
+
+
+        // Binary search on the flattened matrix
+        int left = 0;
+        int right = m * n - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int midElement = matrix[mid / n][mid % n]; // Mapping 1D index to 2D indices
+
+            if (midElement == target) {
+                return true;
+            } else if (midElement < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return false;
+    }
+};
