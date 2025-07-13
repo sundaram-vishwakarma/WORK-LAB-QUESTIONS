f47c7f350d90633f44b64f0fee77084cf73779df
sundaram-vishwakarma <sundaram.sharma@geeksforgeeks.org>
2025-07-13 14:21:58 +0530

    Update LC1823_findTheWinnerOfTheCircularGame.cpp

--- 10-07-2025/LC1823_findTheWinnerOfTheCircularGame.cpp
+++ 10-07-2025/LC1823_findTheWinnerOfTheCircularGame.cpp
@@ -1,1 +1,22 @@
+class Solution {
+public:
+    int findTheWinner(int n, int k) {
+        queue<int> q;
+        for (int i = 1; i <= n; ++i) {
+            q.push(i);
+        }
 
+        int curr = 1;
+        while (!q.empty()) {
+            curr = q.front();
+            q.pop();
+            for (int i = 1; i < k; ++i) {
+                q.push(curr);
+                curr = q.front();
+                q.pop();
+            }
+        }
+
+        return curr;
+    }
+};
