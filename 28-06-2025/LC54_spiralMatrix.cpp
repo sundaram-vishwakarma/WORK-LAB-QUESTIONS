92db91c4069ae919c73511766b02f756c7e4dbe8
sundaram-vishwakarma <sundaram.sharma@geeksforgeeks.org>
2025-07-13 12:35:34 +0530

    Update LC54_spiralMatrix.cpp

--- 28-06-2025/LC54_spiralMatrix.cpp
+++ 28-06-2025/LC54_spiralMatrix.cpp
@@ -1,1 +1,43 @@
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        int startRow = 0;
+        int endRow = matrix.size() - 1;
 
+        int startCol = 0;
+        int endCol = matrix[0].size() - 1;
+
+        // int numberOfElement = matrix.size() * matrix[0].size();
+        vector<int> ans;
+
+        while (startRow <= endRow || startCol <= endCol) {
+            // for starting row;
+            for (int i=startCol; i<=endCol; i++) {
+                ans.push_back(matrix[startCol][i]);
+            }
+            startRow++;
+
+            // for end column
+            for (int i=startRow; i<=endRow; i++) {
+                ans.push_back(matrix[i][endCol]);
+            }
+            endCol--;
+
+            // for end Row
+            while (startCol <= endCol) {        // due to endCol-- previous LINE: 24
+                for (int i=endCol; i>=startCol; i--) {
+                    ans.push_back(matix[endRow][i]);
+                }
+                endRow--;
+            }
+
+            // for starting Col
+            while (startRow <= endRow) {        // due to startRow++ previous LINE: 18
+                for (int i=endRow; i>=startRow; i--) {
+                    ans.push_back(matrix[i][startCol]);
+                }
+                startCol++;
+            }
+        }
+    }
+};
