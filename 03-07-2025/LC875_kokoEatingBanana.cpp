ecb9a8c145bf30b4b9dc0bdaf07d7e7920516a1e
sundaram-vishwakarma <sundaram.sharma@geeksforgeeks.org>
2025-07-13 13:37:40 +0530

    Update LC875_kokoEatingBanana.cpp

--- 03-07-2025/LC875_kokoEatingBanana.cpp
+++ 03-07-2025/LC875_kokoEatingBanana.cpp
@@ -1,1 +1,38 @@
+class Solution {
+private:
+    bool canEatAll(vector<int>& piles, int givenHour, int h) {
+        int actualHour = 0;
+        
+        for(int pile : piles) {
+            actualHour += pile/givenHour;
+            
+            if(pile%givenHour != 0)
+                actualHour++;
+            
+        }
+        
+        return actualHour <= h;
+    }
 
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int n = piles.size();
+        int low = 1;
+        int high = 0;
+        for (int pile : piles) {
+            high = max(high, pile);
+        }
+
+        while (low < high) {
+            int mid = low + (high - low)/2;
+
+            if (canEatAll(piles, mid, h)) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return low;
+    }
+};
