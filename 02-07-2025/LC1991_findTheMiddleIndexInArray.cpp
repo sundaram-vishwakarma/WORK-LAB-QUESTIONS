74fbc18bfd169025ec3a98a73b5f97828a73893b
sundaram-vishwakarma <sundaram.sharma@geeksforgeeks.org>
2025-07-13 12:51:06 +0530

    Update LC1991_findTheMiddleIndexInArray.cpp

--- 02-07-2025/LC1991_findTheMiddleIndexInArray.cpp
+++ 02-07-2025/LC1991_findTheMiddleIndexInArray.cpp
@@ -1,1 +1,23 @@
-
+class Solution {
+public:
+    int findMiddleIndex(vector<int>& nums) {
+        int totalSum = 0;
+        for (int num : nums) {
+            totalSum += num;
+        }
+        
+        int leftSum = 0;
+        
+        for (int i = 0; i < nums.size(); i++) {
+            int rightSum = totalSum - leftSum - nums[i];
+            
+            if (leftSum == rightSum) {
+                return i;
+            }
+            
+            leftSum += nums[i];
+        }
+        
+        return -1;
+    }
+};
