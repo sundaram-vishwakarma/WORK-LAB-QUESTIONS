b8ce561e1c4c7bbf99a6702830c96a59a04761c1
sundaram-vishwakarma <sundaram.sharma@geeksforgeeks.org>
2025-07-13 13:50:39 +0530

    Update LC88_mergeSortedArray.cpp

--- 05-07-2025/LC88_mergeSortedArray.cpp
+++ 05-07-2025/LC88_mergeSortedArray.cpp
@@ -1,1 +1,37 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        if (n == 0) {
+            return;
+        }
 
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+
+        // Print initial nums1 state (for debugging)
+        for (int x : nums1) cout << x << " ";
+        cout << endl;
+
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
+            }
+
+            // Print after each merge step (for debugging)
+            for (int x : nums1) cout << x << " ";
+            cout << endl;
+        }
+
+        // If there are remaining elements in nums2, move them into nums1
+        while (j >= 0) {
+            nums1[k--] = nums2[j--];
+
+            // Print after each merge step (for debugging)
+            for (int x : nums1) cout << x << " ";
+            cout << endl;
+        }
+    }
+};
