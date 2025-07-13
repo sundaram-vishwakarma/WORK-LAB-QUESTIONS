5ec9f3e340b68f55120282a52c11814be04c3d1d
sundaram-vishwakarma <sundaram.sharma@geeksforgeeks.org>
2025-07-13 13:58:44 +0530

    Update LC713_subarrayProductLessThanK.cpp

--- 07-07-2025/LC713_subarrayProductLessThanK.cpp
+++ 07-07-2025/LC713_subarrayProductLessThanK.cpp
@@ -1,1 +1,25 @@
+class Solution {
+public:
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+        if (k <= 1) return 0;  // handle edge case where k <= 1
+
+        int n = nums.size();
+        int result = 0;
+        int prod = 1;
+        int i = 0;
+
+        for (int j = 0; j < n; ++j) {
+            prod *= nums[j];
+
+            while (i <= j && prod >= k) {
+                prod /= nums[i];
+                i++;
+            }
+
+            result += (j - i + 1);
+        }
+
+        return result;
+    }
+};
 
