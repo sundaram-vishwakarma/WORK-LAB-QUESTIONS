6b3de261e1798065d39ef4e87f8e96ba86131c50
sundaram-vishwakarma <sundaram.sharma@geeksforgeeks.org>
2025-07-13 14:20:26 +0530

    Update LC905_sortArrayByParity.cpp

--- 09-07-2025/LC905_sortArrayByParity.cpp
+++ 09-07-2025/LC905_sortArrayByParity.cpp
@@ -1,1 +1,19 @@
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        int i = -1;  // Pointer for the start of the array
+        int j = nums.size() - 1;  // Pointer for the end of the array
 
+        while (i < j) {
+            if (nums[j] % 2 == 0) {  // Even number at the end
+                i++;
+                // Swap the elements
+                swap(nums[i], nums[j]);
+            } else {
+                j--;  // Move the pointer to the left if odd number is found
+            }
+        }
+
+        return nums;  // Return the sorted array
+    }
+};
