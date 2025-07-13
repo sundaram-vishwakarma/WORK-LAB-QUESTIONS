8b0fc3afdc471b064c93aec79ee0e1ab4c6283c5
sundaram-vishwakarma <sundaram.sharma@geeksforgeeks.org>
2025-07-13 14:41:50 +0530

    Update LC83_removeDuplicatesFromSortedList.cpp

--- 12-07-2025/LC83_removeDuplicatesFromSortedList.cpp
+++ 12-07-2025/LC83_removeDuplicatesFromSortedList.cpp
@@ -1,1 +1,20 @@
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if (head == NULL || head->next == NULL) {
+            return head;
+        }
 
+        ListNode* current = head;
+
+        while (current != NULL && current->next != NULL) {
+            if (current->val == current->next->val) {
+                current->next = current->next->next;
+            } else {
+                current = current->next;
+            }
+        }
+
+        return head;
+    }
+};
