4b3f8dc5ac21cf471d49e1fb26c13ceb789d55a9
sundaram-vishwakarma <sundaram.sharma@geeksforgeeks.org>
2025-07-13 14:12:39 +0530

    Update LC769_maxChunksToMakeSorted.cpp

--- 09-07-2025/LC769_maxChunksToMakeSorted.cpp
+++ 09-07-2025/LC769_maxChunksToMakeSorted.cpp
@@ -1,1 +1,18 @@
-
+class Solution {
+public:
+    int maxChunksToSorted(vector<int>& arr) {
+        int maxVal = 0;  // Tracks the maximum value seen so far
+        int chunks = 0;  // Number of chunks
+        
+        for (int i = 0; i < arr.size(); i++) {
+            maxVal = max(maxVal, arr[i]);  // Update the maximum value
+            
+            // If the maximum value is equal to the current index, we can make a chunk
+            if (maxVal == i) {
+                chunks++;
+            }
+        }
+        
+        return chunks;
+    }
+};
