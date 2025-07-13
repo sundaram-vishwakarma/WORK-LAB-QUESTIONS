0dad857afba8a3597229649d53c847ae564ca984
sundaram-vishwakarma <sundaram.sharma@geeksforgeeks.org>
2025-07-13 12:42:43 +0530

    Update LC238_productOfArrayExceptSelf.cpp

--- 30-06-2025/LC238_productOfArrayExceptSelf.cpp
+++ 30-06-2025/LC238_productOfArrayExceptSelf.cpp
@@ -1,1 +1,21 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(n, 1);
 
+        int prefixProduct = nums[0];
+        for (int i=1; i<n; i++) {       // O(n)
+            ans[i] = prefixProduct;
+            prefixProduct *= nums[i];
+        }
+
+        int suffixProduct = nums[n-1];
+        for (int i=n-2; i>=0; i--) {    // O(n)
+            ans[i] *= suffixProduct;
+            suffixProduct *= nums[i];
+        }
+
+        return ans;
+    }
+};
