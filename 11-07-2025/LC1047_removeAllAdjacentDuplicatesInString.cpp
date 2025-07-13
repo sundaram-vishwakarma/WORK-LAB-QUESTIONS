2f5b2aa3bdda67d24c9ea02889d1e550f8cc3f0e
sundaram-vishwakarma <sundaram.sharma@geeksforgeeks.org>
2025-07-13 14:31:07 +0530

    Update LC1047_removeAllAdjacentDuplicatesInString.cpp

--- 11-07-2025/LC1047_removeAllAdjacentDuplicatesInString.cpp
+++ 11-07-2025/LC1047_removeAllAdjacentDuplicatesInString.cpp
@@ -1,1 +1,21 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        stack<char> st;
+        
+        for(int i = S.length()-1; i>=0; i--) {
+            if(st.empty() || st.top() != S[i])
+                st.push(S[i]);
+            else
+                st.pop();
+        }
 
+        string result = "";
+        while(!st.empty()) {
+            result += st.top();
+            st.pop();
+        }
+        return result;
+
+    }
+};
