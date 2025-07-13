2f86d4b69d530ad5a0c7b9027f3e898c736acfda
sundaram-vishwakarma <sundaram.sharma@geeksforgeeks.org>
2025-07-13 14:40:48 +0530

    Update LC61_rotateList.cpp

--- 12-07-2025/LC61_rotateList.cpp
+++ 12-07-2025/LC61_rotateList.cpp
@@ -1,1 +1,39 @@
+class Solution {
+public:
+    int getLength(ListNode* head) {
+        int count = 0;
+        while (head != nullptr) {
+            count++;
+            head = head->next;
+        }
+        return count;
+    }
+
+    ListNode* rotateRight(ListNode* head, int k) {
+        if (!head || !head->next || k == 0) return head;
+
+        int len = getLength(head);
+        k = k % len;
+        if (k == 0) return head;
+
+        // Find the new tail (at position len - k - 1)
+        ListNode* cutNode = head;
+        for (int i = 0; i < len - k - 1; ++i) {
+            cutNode = cutNode->next;
+        }
+
+        // New head is the next node
+        ListNode* newHead = cutNode->next;
+        cutNode->next = nullptr;
+
+        // Go to the end of the new list and connect it to the old head
+        ListNode* tail = newHead;
+        while (tail->next != nullptr) {
+            tail = tail->next;
+        }
+        tail->next = head;
+
+        return newHead;
+    }
+};
 
