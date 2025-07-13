ee7a7d17db3ca045916198f20d4a42212788d289
sundaram-vishwakarma <sundaram.sharma@geeksforgeeks.org>
2025-07-13 12:28:19 +0530

    Update LC75_sortColors.cpp

--- 27-06-2025/LC75_sortColors.cpp
+++ 27-06-2025/LC75_sortColors.cpp
@@ -1,1 +1,29 @@
+class Solution {
+public:
+    void swap(vector<int>& nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
 
+    void sortColors(vector<int>& nums) {
+        int start = 0;
+        int end = nums.size() - 1;
+        int current = 0;
+
+        while (current <= end) {
+            if (nums[current] == 0) {     // try to keep it at start
+                swap(nums, current, start);
+                start++;
+                current++;
+            }
+            else if (nums[current] == 2) {    // try to keep it at end
+                swap(nums, current, end);
+                end--;
+            }
+            else {          // remain as it is in the same place
+                current++;
+            }
+        }
+    }
+};
