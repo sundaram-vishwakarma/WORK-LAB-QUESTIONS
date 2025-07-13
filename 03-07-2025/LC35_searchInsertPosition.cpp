7211263c6b4445191ec44d345aa4f9b006f011e3
sundaram-vishwakarma <sundaram.sharma@geeksforgeeks.org>
2025-07-13 13:30:41 +0530

    Update LC35_searchInsertPosition.cpp

--- 03-07-2025/LC35_searchInsertPosition.cpp
+++ 03-07-2025/LC35_searchInsertPosition.cpp
@@ -1,1 +1,27 @@
+class Solution {
+public:
+    // Helper function to find the insert position using binary search
+    int pos(const vector<int>& nums, int i, int j, int target) {
+        int mid = (i + j) / 2;
+        
+        if (target <= nums[i]) {
+            return i;
+        }
+        if (target > nums[j]) {
+            return j + 1;
+        }
+        if (target == nums[mid]) {
+            return mid;
+        }
+        else if (target < nums[mid]) {
+            return pos(nums, i, mid - 1, target);
+        }
+        else {
+            return pos(nums, mid + 1, j, target);
+        }
+    }
 
+    int searchInsert(vector<int>& nums, int target) {
+        return pos(nums, 0, nums.size() - 1, target);
+    }
+};
