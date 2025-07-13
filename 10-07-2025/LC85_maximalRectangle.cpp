16f0019bafcdfb40ebaea4e733b273378218ec69
sundaram-vishwakarma <sundaram.sharma@geeksforgeeks.org>
2025-07-13 14:27:15 +0530

    Update LC85_maximalRectangle.cpp

--- 10-07-2025/LC85_maximalRectangle.cpp
+++ 10-07-2025/LC85_maximalRectangle.cpp
@@ -1,1 +1,46 @@
+class Solution {
+public:
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        if (matrix.empty() || matrix[0].empty()) {
+            return 0;
+        }
 
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+        vector<int> heights(cols, 0);
+        int maxArea = 0;
+
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                if (matrix[i][j] == '1') {
+                    heights[j] += 1;
+                } else {
+                    heights[j] = 0;
+                }
+            }
+            maxArea = max(maxArea, largestRectangleArea(heights));
+        }
+
+        return maxArea;
+    }
+
+private:
+    int largestRectangleArea(const vector<int>& heights) {
+        stack<int> s;
+        int maxArea = 0;
+        int n = heights.size();
+
+        for (int i = 0; i <= n; ++i) {
+            int height = (i == n) ? 0 : heights[i];
+            while (!s.empty() && heights[s.top()] > height) {
+                int h = heights[s.top()];
+                s.pop();
+                int width = s.empty() ? i : i - s.top() - 1;
+                maxArea = max(maxArea, h * width);
+            }
+            s.push(i);
+        }
+
+        return maxArea;
+    }
+};
