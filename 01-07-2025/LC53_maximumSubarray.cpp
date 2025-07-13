c92b5dd5291df5e470096a284bc7f525630213f3
sundaram-vishwakarma <sundaram.sharma@geeksforgeeks.org>
2025-07-13 12:48:42 +0530

    Update LC53_maximumSubarray.cpp

--- 01-07-2025/LC53_maximumSubarray.cpp
+++ 01-07-2025/LC53_maximumSubarray.cpp
@@ -1,1 +1,14 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int sum =0;
+        int maxSum = nums[0];
 
+        for (int num : nums) {
+            sum = max(sum + num, num);
+            maxSum = max(maxSum, sum);
+        }
+
+        return maxSum;
+    }
+};
