a6a8b79b833dd071ca54fe66cc6349757d976552
sundaram-vishwakarma <sundaram.sharma@geeksforgeeks.org>
2025-07-13 12:47:48 +0530

    Update LC392_isSubsequence.cpp

--- 01-07-2025/LC392_isSubsequence.cpp
+++ 01-07-2025/LC392_isSubsequence.cpp
@@ -1,1 +1,27 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int sLen = s.length();
+        int tLen = t.length();
 
+        if (sLen == 0) {
+            return true;
+        }
+
+        int idxS = 0;
+        int idxT = 0;
+
+        while (idxS<sLen && idxT<tLen) {
+            if (s[idxS] == t[idxT]) {
+                if (idxS == sLen-1) {
+                    return true;
+                }
+                idxS++;
+
+            }
+            idxT++;
+        }
+
+        return false;
+    }
+};
