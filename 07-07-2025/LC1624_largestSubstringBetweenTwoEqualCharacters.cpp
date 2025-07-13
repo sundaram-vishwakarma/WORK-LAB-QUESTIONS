184da248d39552acace2f89bac8853727c285d4c
sundaram-vishwakarma <sundaram.sharma@geeksforgeeks.org>
2025-07-13 13:52:52 +0530

    Update LC1624_largestSubstringBetweenTwoEqualCharacters.cpp

--- 07-07-2025/LC1624_largestSubstringBetweenTwoEqualCharacters.cpp
+++ 07-07-2025/LC1624_largestSubstringBetweenTwoEqualCharacters.cpp
@@ -1,1 +1,20 @@
+class Solution {
+public:
+    int maxLengthBetweenEqualCharacters(string s) {
+        int maxLen = -1;
+        vector<int> firstIndex(26, -1);  // For 26 lowercase English letters
+
+        for (int i = 0; i < s.length(); ++i) {
+            int chIndex = s[i] - 'a';
+            if (firstIndex[chIndex] == -1) {
+                firstIndex[chIndex] = i;  // Store first occurrence
+            } else {
+                int length = i - firstIndex[chIndex] - 1;
+                maxLen = max(maxLen, length);
+            }
+        }
+
+        return maxLen;
+    }
+};
 
