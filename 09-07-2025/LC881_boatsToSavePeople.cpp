d502fdc61eed3d18cd794cb6dba6c970d045087f
sundaram-vishwakarma <sundaram.sharma@geeksforgeeks.org>
2025-07-13 14:17:26 +0530

    Update LC881_boatsToSavePeople.cpp

--- 09-07-2025/LC881_boatsToSavePeople.cpp
+++ 09-07-2025/LC881_boatsToSavePeople.cpp
@@ -1,1 +1,24 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        // Sort the people array
+        sort(people.begin(), people.end());
 
+        int i = 0;  // Pointer to the lightest person
+        int j = people.size() - 1;  // Pointer to the heaviest person
+        int boat = 0;  // Number of boats
+
+        // Greedily try to pair the lightest and heaviest person
+        while (i <= j) {
+            if (people[i] + people[j] <= limit) {
+                i++;  // Lightest person paired with the heaviest
+                j--;  // Decrease the index of the heaviest person
+            } else {
+                j--;  // Heaviest person goes alone
+            }
+            boat++;  // One more boat is used
+        }
+
+        return boat;
+    }
+};
