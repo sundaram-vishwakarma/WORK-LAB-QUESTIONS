51b491a4e03b311c5cf67eb4aaeff7d7c7364fe6
sundaram-vishwakarma <sundaram.sharma@geeksforgeeks.org>
2025-07-13 14:35:39 +0530

    Update LC739_dailyTemperatures.cpp

--- 11-07-2025/LC739_dailyTemperatures.cpp
+++ 11-07-2025/LC739_dailyTemperatures.cpp
@@ -1,1 +1,20 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        int n = temperatures.size();
+        vector<int> answer(n, 0);
+        stack<int> st; // Stack to store indices
+
+        for (int i = 0; i < n; ++i) {
+            while (!st.empty() && temperatures[i] > temperatures[st.top()]) {
+                int prevIndex = st.top();
+                st.pop();
+                answer[prevIndex] = i - prevIndex;
+            }
+            st.push(i);
+        }
+
+        return answer;
+    }
+};
 
