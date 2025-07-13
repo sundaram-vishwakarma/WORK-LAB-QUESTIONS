c90552c5c60434d1277e3a7d308d57ea6ea44bc3
sundaram-vishwakarma <sundaram.sharma@geeksforgeeks.org>
2025-07-13 13:57:28 +0530

    Update LC409_longestPalindrome.cpp

--- 07-07-2025/LC409_longestPalindrome.cpp
+++ 07-07-2025/LC409_longestPalindrome.cpp
@@ -1,1 +1,28 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        unordered_map<char, int> freqMap;
 
+        // Count frequency of each character
+        for (char c : s) {
+            freqMap[c]++;
+        }
+
+        int length = 0;
+        bool hasOdd = false;
+
+        for (auto& entry : freqMap) {
+            int count = entry.second;
+            length += (count / 2) * 2;
+            if (count % 2 == 1) {
+                hasOdd = true;
+            }
+        }
+
+        if (hasOdd) {
+            length += 1;
+        }
+
+        return length;
+    }
+};
