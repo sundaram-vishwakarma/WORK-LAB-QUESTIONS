b9a50ec269363b6fa94aabf39c3ee7c7d4fd7e38
sundaram-vishwakarma <sundaram.sharma@geeksforgeeks.org>
2025-07-13 14:39:18 +0530

    Update LC328_oddEvenLinkedList.cpp

--- 12-07-2025/LC328_oddEvenLinkedList.cpp
+++ 12-07-2025/LC328_oddEvenLinkedList.cpp
@@ -1,1 +1,26 @@
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if (head == NULL || head->next == NULL) {
+            return head;
+        }
+
+        ListNode* odd = head;
+        ListNode* even = head->next;
+
+        ListNode* startEven = head->next;
+
+        while (even != NULL && even->next != NULL) {
+            odd->next = even->next;
+            even->next = even->next->next;
+
+            odd = odd->next;
+            even = even->next;
+        }
+
+        odd->next = startEven;
+
+        return head;
+    }
+};
 
