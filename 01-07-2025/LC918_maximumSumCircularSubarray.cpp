858a4d654f79e4f3bc3a63e44f3d8d30faf93688
sundaram-vishwakarma <sundaram.sharma@geeksforgeeks.org>
2025-07-13 12:50:01 +0530

    Update LC918_maximumSumCircularSubarray.cpp

--- 01-07-2025/LC918_maximumSumCircularSubarray.cpp
+++ 01-07-2025/LC918_maximumSumCircularSubarray.cpp
@@ -1,1 +1,47 @@
+class Solution {
+private: 
+    int kadanesMax(vector<int>& nums) {
+        int sum = 0;
+        int maxSum = nums[0];
 
+        for (int num : nums) {
+            sum = max(sum + num, num);
+            maxSum = max(maxSum, sum);
+        }
+
+        return maxSum;
+    }
+
+private:
+    int kadanesMin(vector<int>& nums) {
+        int sum = 0;
+        int minSum = nums[0];
+
+        for (int num : nums) {
+            sum = min(sum + num, num);
+            minSum = min(minSum, sum);
+        }
+
+        return minSum;
+    }
+
+public:
+    int maxSubarraySumCircular(vector<int>& nums) {
+        int totalSum = 0;
+        for (int num : nums) {
+            totalSum += num;
+        }
+
+        int maxSum = kadanesMax(nums);
+
+        int minSum = kadanesMin(nums);
+
+        int circSum = totalSum - minSum;
+
+        if (maxSum > 0) {
+            return max(maxSum, circSum);
+        }
+
+        return maxSum;
+    }
+};
