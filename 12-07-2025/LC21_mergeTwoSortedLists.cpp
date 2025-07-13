5cdc9a84aebb007bc63c77660dc274bfda15c6dd
sundaram-vishwakarma <sundaram.sharma@geeksforgeeks.org>
2025-07-13 14:36:55 +0530

    Update LC21_mergeTwoSortedLists.cpp

--- 12-07-2025/LC21_mergeTwoSortedLists.cpp
+++ 12-07-2025/LC21_mergeTwoSortedLists.cpp
@@ -1,1 +1,28 @@
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* ans = new ListNode(0); // Dummy node
+        ListNode* current = ans;
+
+        while (list1 != nullptr && list2 != nullptr) {
+            if (list1->val < list2->val) {
+                current->next = list1;
+                list1 = list1->next;
+            } else {
+                current->next = list2;
+                list2 = list2->next;
+            }
+            current = current->next;
+        }
+
+        // Attach the remaining nodes
+        if (list1 != nullptr) {
+            current->next = list1;
+        } else {
+            current->next = list2;
+        }
+
+        return ans->next; // Return the head of the merged list
+    }
+};
 
