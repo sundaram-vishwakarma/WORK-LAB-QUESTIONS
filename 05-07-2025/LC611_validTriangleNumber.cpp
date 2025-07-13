336c12e50d7b770f81aa3e098c3aeb44b29d0bb5
sundaram-vishwakarma <sundaram.sharma@geeksforgeeks.org>
2025-07-13 13:46:39 +0530

    Update LC611_validTriangleNumber.cpp

--- 05-07-2025/LC611_validTriangleNumber.cpp
+++ 05-07-2025/LC611_validTriangleNumber.cpp
@@ -1,1 +1,78 @@
+///// Brute Force Approach
+class Solution {
+public:
+    int triangleNumber(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        
+        int count = 0;
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = i + 1; j < n - 1; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    if (nums[i] + nums[j] > nums[k]) {
+                        count++;
+                    }
+                }
+            }
+        }
 
+        return count;
+    }
+};
+
+
+
+
+
+///// Some Optimized Approach
+class Solution {
+public:
+    int triangleNumber(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+
+        int count = 0;
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = i + 1; j < n - 1; j++) {
+                for (int k = n - 1; k >= j + 1; k--) {
+                    if (nums[i] + nums[j] > nums[k]) {
+                        count += k - j;
+                        break;
+                    }
+                }
+            }
+        }
+
+        return count;
+    }
+};
+
+
+
+
+
+
+//// Most Optimized Approach
+class Solution {
+public:
+    int triangleNumber(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+
+        int count = 0;
+        for (int k = n - 1; k >= 2; k--) {
+            int i = 0;
+            int j = k - 1;
+            while (i < j) {
+                if (nums[i] + nums[j] > nums[k]) {
+                    count += j - i;
+                    j--;
+                } else {
+                    i++;
+                }
+            }
+        }
+
+        return count;
+    }
+};
