5ca7706185723f4701053204cf4a380acf0ed4d4
sundaram-vishwakarma <sundaram.sharma@geeksforgeeks.org>
2025-07-13 13:38:50 +0530

    Update LC03_longestSubstringWithoutRepeatingCharacter.cpp

--- 04-07-2025/LC03_longestSubstringWithoutRepeatingCharacter.cpp
+++ 04-07-2025/LC03_longestSubstringWithoutRepeatingCharacter.cpp
@@ -1,1 +1,21 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int n = s.length();
+        int maxLength = 0;
+        int left = 0, right = 0;
+        unordered_set<char> seen;
 
+        while (right < n) {
+            if (seen.find(s[right]) == seen.end()) {
+                seen.insert(s[right]);
+                right++;
+                maxLength = max(maxLength, right - left);
+            } else {
+                seen.erase(s[left]);
+                left++;
+            }
+        }
+        return maxLength;
+    }
+};
