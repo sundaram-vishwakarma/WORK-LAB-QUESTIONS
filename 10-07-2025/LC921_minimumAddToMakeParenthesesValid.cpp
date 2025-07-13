31da697229737405d7f0ee637080091c8bd7b144
sundaram-vishwakarma <sundaram.sharma@geeksforgeeks.org>
2025-07-13 14:28:18 +0530

    Update LC921_minimumAddToMakeParenthesesValid.cpp

--- 10-07-2025/LC921_minimumAddToMakeParenthesesValid.cpp
+++ 10-07-2025/LC921_minimumAddToMakeParenthesesValid.cpp
@@ -1,1 +1,20 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        stack<char> st;
 
+        for (char c : s) {
+            if (c == '(') {
+                st.push(c);
+            } else {
+                if (!st.empty() && st.top() == '(') {
+                    st.pop();
+                } else {
+                    st.push(')');
+                }
+            }
+        }
+
+        return st.size();
+    }
+};
