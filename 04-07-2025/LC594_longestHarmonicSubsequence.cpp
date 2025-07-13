4441131a034114a6a134fe05fc919c243d0bd276
sundaram-vishwakarma <sundaram.sharma@geeksforgeeks.org>
2025-07-13 13:40:58 +0530

    Update LC594_longestHarmonicSubsequence.cpp

--- 04-07-2025/LC594_longestHarmonicSubsequence.cpp
+++ 04-07-2025/LC594_longestHarmonicSubsequence.cpp
@@ -1,1 +1,22 @@
+class Solution {
+public:
+    int findLHS(vector<int>& nums) {
+        unordered_map<int, int> map;
+        int result = 0;
 
+        // Count frequency of each number
+        for (int num : nums) {
+            map[num]++;
+        }
+
+        // Check for each number if num + 1 exists
+        for (auto& pair : map) {
+            int num = pair.first;
+            if (map.count(num + 1)) {
+                result = max(result, pair.second + map[num + 1]);
+            }
+        }
+
+        return result;
+    }
+};
