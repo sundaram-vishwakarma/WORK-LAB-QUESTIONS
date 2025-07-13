44789ae06e71fb9c8a0b5b0bd68f43dbf57c8084
sundaram-vishwakarma <sundaram.sharma@geeksforgeeks.org>
2025-07-13 12:38:20 +0530

    Update LC73_SetMatrixZeros.cpp

--- 28-06-2025/LC73_SetMatrixZeros.cpp
+++ 28-06-2025/LC73_SetMatrixZeros.cpp
@@ -1,1 +1,37 @@
+#include <vector>
+#include <unordered_set>
 
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        unordered_set<int> rows;
+        unordered_set<int> cols;
+
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        // Identify the rows and columns that need to be set to zero
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    rows.insert(i);
+                    cols.insert(j);
+                }
+            }
+        }
+
+        // Set the identified rows to zero
+        for (int row : rows) {
+            for (int j = 0; j < n; j++) {
+                matrix[row][j] = 0;
+            }
+        }
+
+        // Set the identified columns to zero
+        for (int col : cols) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][col] = 0;
+            }
+        }
+    }
+};
