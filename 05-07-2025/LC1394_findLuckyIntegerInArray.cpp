33c022d1e8d0c38a1982fac4480b69cc564926ac
sundaram-vishwakarma <sundaram.sharma@geeksforgeeks.org>
2025-07-13 13:42:53 +0530

    Update LC1394_findLuckyIntegerInArray.cpp

--- 05-07-2025/LC1394_findLuckyIntegerInArray.cpp
+++ 05-07-2025/LC1394_findLuckyIntegerInArray.cpp
@@ -1,1 +1,29 @@
+class Solution {
+public:
+    int findLucky(vector<int>& arr) {
+        int maxEle = 1;
 
+        // Find the maximum element in the array
+        for (int a : arr) {
+            maxEle = max(maxEle, a);
+        }
+
+        // Create a frequency array to count occurrences of each element
+        vector<int> freq(maxEle + 1, 0);
+
+        // Count the frequency of each element in the input array
+        for (int a : arr) {
+            freq[a]++;
+        }
+
+        // Check from maxEle down to 1 to find the lucky number
+        for (int i = maxEle; i >= 1; i--) {
+            if (i == freq[i]) {
+                return i;
+            }
+        }
+
+        // If no lucky number is found, return -1
+        return -1;
+    }
+};
